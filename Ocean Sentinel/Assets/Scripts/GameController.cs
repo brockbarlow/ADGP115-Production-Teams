95edb2514827b33803fecfe3eb5b7c4a7b123abd
Austin Morrell <austinlm19@gmail.com>
2016-05-09 16:50:35 -0500

    Game Controller not finished

---
+++ Ocean Sentinel/Assets/Scripts/GameController.cs
@@ -0,0 +1,108 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+    // This is the Enemy Prefab.
+    [SerializeField]
+    private GameObject EnemyPre;
+
+    // This is the wait before the while loop in the couroutine begins.
+    private float startWait;
+    // This is the number of enemies that will be spawned.
+    private float numbEnemies;
+    // This is the amount of time between each enemy being spawned.
+    private float spawnWait;
+    // This is the amount of Gold the Player has.
+
+
+    // Determinds if the scene can be restarted.
+    private bool restart;
+    // Is the game over?
+    private bool gameOver;
+    // Show I spawn the next wave?
+    private bool spawnWave;
+
+    // The UI text element for restart.
+    [SerializeField]
+    private Text restartText;
+    // The UI text element for game over.
+    [SerializeField]
+    private Text gameOverText;
+
+    // These are the power-up buttons.
+    [SerializeField]
+    private Button UIButton1;
+    [SerializeField]
+    private Button UIButton2;
+    [SerializeField]
+    private Button UIButton3;
+    [SerializeField]
+    private Button UIButton4;
+
+    void Start ()
+    {
+        startWait = 3;
+        spawnWait = 1;
+        numbEnemies = 5;
+
+        restart = false;
+        gameOver = false;
+        spawnWave = true;
+
+        restartText.text = "";
+        gameOverText.text = "";
+
+        UIButton1.interactable = false;
+        UIButton2.interactable = false;
+        UIButton3.interactable = false;
+        UIButton4.interactable = false;
+
+        StartCoroutine(SpawnWaves());
+	}
+	
+	void Update ()
+    {
+        if (restart)
+        {
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
+
+        if (gameOver)
+        {
+            restartText.text = "Press 'R' for Restart";
+            restart = true;
+        }
+    }
+
+    // This couroutine will spawn the enemies in the game.
+    IEnumerator SpawnWaves()
+    {
+        // I wait a sec before starting this while loop. Why? For courtesy, the player is about to be stormed with wave after wave of enemies!
+        yield return new WaitForSeconds(startWait);
+        // Spawn the wave.
+        while(spawnWave)
+        {
+            for (int i = 0; i < numbEnemies; i++)
+            {
+                Vector3 spawnPosition = new Vector3();
+                Quaternion spawnRotation = Quaternion.identity;
+                Instantiate(EnemyPre, spawnPosition, spawnRotation);
+                yield return new WaitForSeconds(spawnWait);
+            }
+            // Pause the couroutine.
+            spawnWave = false;
+        }
+    }
+
+    public void GameOver()
+    {
+        gameOverText.text = "Game Over!";
+        gameOver = true;
+    }
+}
