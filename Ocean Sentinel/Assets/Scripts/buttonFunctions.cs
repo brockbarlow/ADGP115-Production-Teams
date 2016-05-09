7a7cbc7986f9f565520b68a47f63a0dc57b74719
brockbarlow <brock.barlow@students.aie.edu.au>
2016-05-09 16:47:54 -0500

    commited scenes, materials, and scripts

---
+++ Ocean Sentinel/Assets/Scripts/buttonFunctions.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+//this script will give buttons things to do when clicked.;
+
+public class buttonFunctions : MonoBehaviour {
+
+	public void startGame() //this function is used to load the game scene.
+    {                       //this function will also be used for the restart button.
+        SceneManager.LoadScene("Game");
+    }
+
+    public void exitGame() //this function is used to exit the game.
+    {
+        Application.Quit();
+    }
+}
