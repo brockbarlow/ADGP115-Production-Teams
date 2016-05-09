4e52bad519585c218d02b92e64fe094acb6963d2
Eugene Hochenedel <eugeneatschool@gmail.com>
2016-05-09 17:57:35 -0500

    Movement Experimentation Began experimenting with input for movement

--- Ocean Sentinel/Assets/Scripts/PlayerMovement.cs
+++ Ocean Sentinel/Assets/Scripts/PlayerMovement.cs
@@ -16,13 +16,13 @@ public class PlayerMovement : MonoBehaviour
 	{
 		//All movement should be in a circular motion around a game object designated as the base
 		//Allows for movement of the Player gameObject using 'A' & 'D' or the 'left' & 'right' arrowkeys
-		float Movement = Input.GetAxis("Horizontal") * MoveVelocity;
-
-		float TravelArc = Mathf.LerpAngle(0, 360, Time.time);
-		transform.eulerAngles = new Vector3(0, TravelArc, 0);
 		
-		Vector3 speed = new Vector3(Movement, 0, 0);
-		speed = transform.eulerAngles.y * speed;
+
+		float TravelArc = Mathf.LerpAngle(0, 359, 0);
+		//transform.eulerAngles = new Vector3(0, TravelArc, 0);
+		float Movement = Input.GetAxis("Horizontal") * MoveVelocity;
+		Vector3 speed = new Vector3(Movement + TravelArc, 0, 0);
+		//speed = TravelArc * speed;
 
 		CharacterController playerMotion = GetComponent<CharacterController>();
 		playerMotion.Move(speed);
