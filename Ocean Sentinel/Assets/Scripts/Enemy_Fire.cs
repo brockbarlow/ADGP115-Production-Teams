4ab83c451f4943dbb6da74159092f9a73a5b43c7
Austin Morrell <austinlm19@gmail.com>
2016-05-10 11:36:43 -0500

    Base, EnemyFire, Pickups, Upgrades

---
+++ Ocean Sentinel/Assets/Scripts/Enemy_Fire.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy_Fire : MonoBehaviour {
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Base")
+        {
+            other.gameObject.GetComponent<Base>().HP -= 25;
+        }
+    }
+}
