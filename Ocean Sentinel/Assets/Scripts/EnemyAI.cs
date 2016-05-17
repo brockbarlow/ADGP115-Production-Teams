9a878399c360a0e62739e41867c4fc6786f9909c
WillMon <w.montero34@gmail.com>
2016-05-17 15:53:11 -0500

    Commiting Completed Scripts

--- Ocean Sentinel/Assets/Scripts/EnemyAI.cs
+++ Ocean Sentinel/Assets/Scripts/EnemyAI.cs
@@ -10,49 +10,63 @@ using System.Collections;
 public class EnemyAI : MonoBehaviour {
 
     // Players Transformer Will be used to track the players location 
-    Transform baseTransformer;
-    GameObject @base;
-
+    Transform @base;
     //Instance so i can reffer wether or not it has found the wall.
-    GameObject wall;
+    public GameObject Prefab;
+    //Sets the location of the wanted projectile 
+    public GameObject spawnPoint;
+    //Time between projectile  
+    public float time; 
+    //Time till next shot 
+    public float  timeCap = 5;
+    //Checks when the enemy collides with the wall 
+    bool trigger = false;
+    //Referres to the games naviagtion mesh 
     NavMeshAgent navMash;
-     
-	// Use this for initialization
+	//Use this for initialization
 	void Start () {
-        @base = GameObject.FindGameObjectWithTag("Base");
-
-        baseTransformer = @base.transform;
-
-        wall = GameObject.FindGameObjectWithTag("Wall");
+        @base = GameObject.FindGameObjectWithTag("Base").transform;
 
         navMash = GetComponent<NavMeshAgent>();
-
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
         Locate();
+        if (trigger)
+            EnemyShot();
 	}
 
     // Looks for base unless it exists or the health is greater then 0
-    void Locate()
-    {
+    void Locate() {
         if(@base != null)
-<<<<<<< HEAD
             if(@base.GetComponent<Base>().HP  > 0)
-                navMash.SetDestination(baseTransformer.position); 
-=======
-            if(@base.GetComponent<BaseHPScript>().baseCurrentHP  > 0)
                 navMash.SetDestination(@base.position); 
->>>>>>> refs/remotes/brockbarlow/master
         
     }
     // Check for collider that the object comes in contact with 
     void OnTriggerEnter(Collider collision) {
-        if (collision.gameObject == wall)
-        {
+        if (collision.gameObject.tag == "Wall") {
             navMash.enabled = false;
-            Debug.Log("Hit");
+            trigger = true;   
         }
+        else
+            navMash.enabled = true;
+    }
+
+    //Instantiate the projectile if it meet 
+    //If the time meets the time cap it creats another projectile 
+    void EnemyShot() {
+
+        if (time > timeCap){
+            //Creats a new object/sets the location pined by the user
+            Instantiate(Prefab);
+            Prefab.transform.position = spawnPoint.transform.position + spawnPoint.transform.right;
+            Prefab.transform.localRotation = spawnPoint.transform.rotation;
+            Prefab.transform.Rotate(spawnPoint.transform.rotation.z, spawnPoint.transform.rotation.x, 90);
+            time = 0;
+        }
+        //Calculates the time between shots 
+        time += 3 * Time.deltaTime;  
     }
 }
