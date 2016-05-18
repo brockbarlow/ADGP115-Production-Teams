eae2b1d3ff27bce2d5c481665a2a7d8ff663173b
brockbarlow <brock.barlow@students.aie.edu.au>
2016-05-18 13:51:23 -0500

    end of day commit

--- Ocean Sentinel/Assets/Scripts/SpawnMoverVertical.cs
+++ Ocean Sentinel/Assets/Scripts/SpawnMoverVertical.cs
@@ -5,9 +5,8 @@ using System.Collections;
 
 public class SpawnMoverVertical : MonoBehaviour
 {
-
-    //public float speed; //how fast the spawn points will move
-    private bool down, up = false; //if the spawn points can move or not
+    private bool down = false; //if the spawn points can move or not
+    private bool up = true;
 
     // Use this for initialization
     void Start()
@@ -18,6 +17,30 @@ public class SpawnMoverVertical : MonoBehaviour
     void Lerp(float a, float b, float c)
     {
         gameObject.transform.position = new Vector3(transform.position.x + a, transform.position.y + b, transform.position.z + c);
+    }
+
+    //void OnTriggerEnter(Collider collision)
+    //{
+    //    //check the collision between the pojectile and the bass
+    //    if (collision.gameObject.tag == "Base")
+    //    {
+    //        Health.HP -= dmg;
+    //        Destroy(gameObject);
+    //    }
+
+    void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.tag == "SpawnWall")
+        {
+            up = true;
+            down = false;
+        }
+
+        if (collision.gameObject.tag == "SpawnWall2")
+        {
+            up = false;
+            down = true;
+        }
     }
 
     // Update is called once per frame
@@ -33,16 +56,16 @@ public class SpawnMoverVertical : MonoBehaviour
             Lerp(0, 0, -8 * Time.deltaTime /** speed*/);
         }
 
-        if (gameObject.transform.position.z <= -12)
-        {
-            up = true;
-            down = false;
-        }
+        //if (gameObject.transform.position.z <= -12)
+        //{
+        //    up = true;
+        //    down = false;
+        //}
 
-        if (gameObject.transform.position.z >= 35)
-        {
-            up = false;
-            down = true;
-        }
+        //if (gameObject.transform.position.z >= 35)
+        //{
+        //    up = false;
+        //    down = true;
+        //}
     }
 }
