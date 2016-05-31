dc204f78caef58c26768c31162fa0de40b643295
WillMon <w.montero34@gmail.com>
2016-05-31 16:38:04 -0500

    Will Mon 9th Commit 05/31/2016 4:00pm
    Added: Audio/ EnemyDeath.wav  & BaseHit.mp3
    
    Edited: Script/Base made it so that on base collision with the
    gameObject taged Projectile it willl play BaseHit.mp3
    Script/EnemyAI Staged so that I can comeback and continue my audio so
    when enemy dies it plays a death clip.

--- Ocean Sentinel/Assets/Scripts/Base.cs
+++ Ocean Sentinel/Assets/Scripts/Base.cs
@@ -1,21 +1,27 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Base : MonoBehaviour {
+public class Base : MonoBehaviour
+{
 
     public float max_HP;
     public float HP;
     public float max_Armor;
     public float Armor;
     public GameController GC;
+    //referes to a game audio sorce with a clip in the gameopbject
+    AudioSource hitAudio;
 
-	void Start () {
+
+    void Start()
+    {
         GC = FindObjectOfType<GameController>();
+        hitAudio = gameObject.GetComponent<AudioSource>();
         max_HP = 300;
         max_Armor = 200;
         HP = max_HP;
         Armor = max_Armor;
-	}
+    }
 
     void Update()
     {
@@ -29,5 +35,13 @@ public class Base : MonoBehaviour {
             HP = 0;
             GC.GameOver();
         }
+
+
+
+    }
+    //Looks for in comming collider with sposific tag then does STUFF
+    void OnTriggerEnter(Collider collision) {
+        if (collision.gameObject.tag == "Projectile")
+            hitAudio.Play();
     }
 }
