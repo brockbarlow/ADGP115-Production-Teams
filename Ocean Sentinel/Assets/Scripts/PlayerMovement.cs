fb3e0c479159e6920d388f88a9cfc8ed76433911
Eugene Hochenedel <eugeneatschool@gmail.com>
2016-06-13 10:23:07 -0500

    Movement When Paused Adjusted the player's controls so they can't move while the game is paused.

--- Ocean Sentinel/Assets/Scripts/PlayerMovement.cs
+++ Ocean Sentinel/Assets/Scripts/PlayerMovement.cs
@@ -20,7 +20,7 @@ public class PlayerMovement : MonoBehaviour
 	public float projectileRate;
 	//A constant value that's used as the base time inbetween each projectile
 	public float newRate;
-	//
+	//A changing value that will be used to check if a projectile can be instantiated
 	private float nextProjectile;
 	//Accesses the Base class
 	Base Defend;
@@ -29,7 +29,7 @@ public class PlayerMovement : MonoBehaviour
 	//Provides access to the Game Controller script
 	GameController GC;
 
-	//
+	//Function that's used to instantiate a projectile object with a velocity 
 	void FireProjectile()
 	{
 		//Instantiates a game object by loading a prefab located in the Resources folder
@@ -37,7 +37,7 @@ public class PlayerMovement : MonoBehaviour
 		
 		//Performs the PlaySound function from the GameController script on instantiation
 		GC.PlaySound(2, 0.45f, 1.5f);
-		//sfx.clip = Resources.Load("Shot3", typeof(AudioClip)) as AudioClip;
+		
 		//
 		Rigidbody rbShot = FindObjectOfType<Rigidbody>();
 
@@ -74,29 +74,45 @@ public class PlayerMovement : MonoBehaviour
 		{
 			projectileRate = 0.0625f;
 		}
+
+
+
 		//All movement should be in a circular motion around a game object designated as the base
 		//Allows for movement of the Player gameObject using 'A' & 'D' or the 'left' & 'right' arrow keys
-		float Movement = Input.GetAxis("Horizontal") * MoveVelocity;
-		transform.RotateAround(Defend.transform.position, Vector3.up, Movement * 4);
 
+		float Movement = Input.GetAxis("Horizontal") * MoveVelocity;
+		
 		//Sets up movent to allow for a limited distance the player can move away from the base
 		//Allows for movement of the Player gameObject using 'W' & 'S' or the 'up' & 'down' arrow keys
 		float adjustDistance = Input.GetAxis("Vertical") * MoveVelocity;
+		
+		//Float that's used to determine the amount of time inbetween each shot.
+		nextProjectile += Time.deltaTime;
 
-		//Connects how the player will move with vertical input to their rotation
-		Vector3 radialMotion = transform.localRotation * new Vector3(adjustDistance, 0, 0);
-		CharacterController distanceControl = GetComponent<CharacterController>();
-
-		//Checks the distance between the player and the base
-		//If the distance is less than four then the player can move with vertical input
-		//But if the distance is greater than four then the player can only move towards the base
-		if (Vector3.Distance(Defend.transform.position, transform.position) < 6 ||
-			Vector3.Distance(Defend.transform.position, transform.position) > 6 &&
-			Input.GetAxis("Vertical") < 0)
+		if (Time.deltaTime != 0)
 		{
-			distanceControl.Move(radialMotion);
-		}
+			//Cirularizes the player's movement to be around the base game object
+			transform.RotateAround(Defend.transform.position, Vector3.up, Movement * 4);
 
+			//Connects how the player will move with vertical input to their rotation
+			Vector3 radialMotion = transform.localRotation * new Vector3(adjustDistance, 0, 0);
+			CharacterController distanceControl = GetComponent<CharacterController>();
+
+			//Checks the distance between the player and the base
+			//If the distance is less than four then the player can move with vertical input
+			//But if the distance is greater than four then the player can only move towards the base
+			if (Vector3.Distance(Defend.transform.position, transform.position) < 6 ||
+				Vector3.Distance(Defend.transform.position, transform.position) > 6 &&
+				Input.GetAxis("Vertical") < 0)
+			{
+				distanceControl.Move(radialMotion);
+			}
+			if (Input.GetButton("Fire1") && nextProjectile > projectileRate && ES.currentSelectedGameObject == null)
+			{
+				nextProjectile = 0;
+				FireProjectile();
+			}
+		}
 		//Changes the Ship object's localRotation about the Y-axis to 180
 		if (Input.GetAxis("Horizontal") > 0 && Ship.transform.rotation.y < 1)
 		{
@@ -108,20 +124,5 @@ public class PlayerMovement : MonoBehaviour
 		{
 			Ship.transform.localRotation = Quaternion.Euler(0, 0, 0);
 		}
-		//Debug.Log(transform.localRotation);
-
-		//
-		nextProjectile += Time.deltaTime;
-		if (Input.GetButton("Fire1") && nextProjectile > projectileRate && ES.currentSelectedGameObject == null /*&& availiableBullets > 0*/)
-		{
-			nextProjectile = 0;
-			FireProjectile();
-		}
-		//Debug.Log(nextProjectile);
-		//if(availiableBullets <= 0)
-		//{
-		//	nextProjectile += 1;
-		//	availiableBullets = nextProjectile;
-		//}
 	}
 }
