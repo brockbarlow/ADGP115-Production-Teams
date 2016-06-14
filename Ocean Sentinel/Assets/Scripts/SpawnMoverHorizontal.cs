f9196a4175116b166265bc35152f6c8d15da3d97
Eugene Hochenedel <eugeneatschool@gmail.com>
2016-06-14 11:59:44 -0500

    Adjusted Spawns Increased the distance the spawn points will be able to move. Adjusted the size of the plane and positions of spawn points.

--- Ocean Sentinel/Assets/Scripts/SpawnMoverHorizontal.cs
+++ Ocean Sentinel/Assets/Scripts/SpawnMoverHorizontal.cs
@@ -25,20 +25,20 @@ public class SpawnMoverHorizontal : MonoBehaviour
         if (forth == true && Time.deltaTime > 0) //if the forth variable is equal to true, the game objects's x position will become positive over
         {                  //time
             Lerp(0.4f, 0, 0);
-        }       // x,  y,  z times time
+        }		// x,  y,  z
 
         if (back == true && Time.deltaTime > 0) //if the back variable is equal to true, the game object's x position will become negative over time.
         {
             Lerp(-0.4f, 0, 0);
-        }       // x,  y,  z  times time
+        }		// x,  y,  z
 
-        if (gameObject.transform.localPosition.x <= -29) //if the game object's x position is less than or equal to -29, set the 
+        if (gameObject.transform.localPosition.x <= -60) //if the game object's x position is less than or equal to -29, set the 
         {                                           //forth variable to true and set the back variable to false.
             forth = true;
             back = false;
         }
 
-        if (gameObject.transform.localPosition.x >= 29) //if the game objects's x position is greater than or equal to 29, set the 
+        if (gameObject.transform.localPosition.x >= 60) //if the game objects's x position is greater than or equal to 29, set the 
         {                                          //forth variable to false and set the back variable to true.
             forth = false;
             back = true;
